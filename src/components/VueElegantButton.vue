485ff0f486f6eb48d50b0eb8a8f979d83514ab0f
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2024-03-11 10:43:02 +0630

    change button design

--- src/components/VueElegantButton.vue
+++ src/components/VueElegantButton.vue
@@ -1,29 +1,32 @@
 <template>
-	<button
-		:style="{
-			padding: '15px',
-			fontSize: '20px',
-			background: background,
-			color: color,
-		}"
-	>
-		{{ text }}
-	</button>
+  <button
+    :style="{
+      padding: '15px',
+      fontSize: '25px',
+      background: background,
+      color: color,
+      borderRadius: '30px',
+      border: 'none',
+      boxShadow: '3px 8px #c2c2c2',
+    }"
+  >
+    {{ text }}
+  </button>
 </template>
 
 <script>
 export default {
-	name: "VueElegantButton",
-	props: {
-		background: {
-			default: "#456099",
-		},
-		color: {
-			default: "#fff",
-		},
-		text: {
-			default: "My Button",
-		},
-	},
+  name: "VueElegantButton",
+  props: {
+    background: {
+      default: "#456099",
+    },
+    color: {
+      default: "#fff",
+    },
+    text: {
+      default: "My Button",
+    },
+  },
 };
 </script>
